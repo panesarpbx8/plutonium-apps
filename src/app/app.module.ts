3cd91a995239c1910e9b6426e63e16d78f0c9903
panesarpbx8 <panesar.pb08@gmail.com>
2022-10-19 11:35:29 -0400

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from "@angular/core";
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from "src/environments/environment";
+
+const config = {
+  apiKey: "AIzaSyAhtPk6Z8cs3-pODGzi06ntNstJJhUghWo",
+  authDomain: "plutonium-dev.firebaseapp.com",
+  projectId: "plutonium-dev",
+  storageBucket: "plutonium-dev.appspot.com",
+  messagingSenderId: "646715865874",
+  appId: "1:646715865874:web:74ec7a6ed2672b672a91cb",
+  measurementId: "G-93TF1DSJ0T"
+};
 
 @NgModule({
   imports: [
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp(config)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { enableProdMode } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { elements } from './app/elements';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+async function init() {
+  const app = await platformBrowserDynamic().bootstrapModule(AppModule);
+  const injector = app.injector;
+
+  for (const [selector, component] of elements) {
+    const element = createCustomElement(component, { injector });
+    customElements.define(selector, element);
+  }
+}
+
+init();
